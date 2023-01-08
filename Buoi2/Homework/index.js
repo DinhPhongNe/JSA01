ed55a38e56d26209b8f3cb1e532f7f48e510a85e
DinhPhongNe <phongvo2405@gmail.com>
2023-01-08 11:58:54 +0700

    Upload

---
+++ Buoi2/Homework/index.js
@@ -0,0 +1,15 @@
+var myDog = {
+    name: "Ngáo",
+    legs: 4,
+    friends: ["everything!"]
+}
+console.log(myDog);
+
+//update
+myDog.name = "Husky"
+
+//create
+myDog.color = "Brown"
+
+//delete
+delete myDog.friends
